5cfbbce7b64ae99a5d2492a850acaccb44e32a14
956zs <kupp59371@gmail.com>
2024-11-13 21:02:09 +0800

    done

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    //IEEE-754 f32-> 單精度 f64->雙精度
+}
+
+fn main2() {
+    let sum = 5 + 10;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; //  -1
+
+    let remainder = 43 % 5;
+}
